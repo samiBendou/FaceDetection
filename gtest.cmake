02bffd5de988adc33f84b9c695e53a6ac6c0d648
bendou <sami.dahoux@etu.emse.fr>
2019-01-24 13:51:03 +0100

    fix: cleaning CMake

--- gtest.cmake
+++ gtest.cmake
@@ -8,10 +8,15 @@ include_directories(SYSTEM
 set(GOOGLETEST_SOURCES
         ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
         ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
-        stb/stb_image.h)
+        stb/stb_image.h stb/stb_image_resize.h)
 
 foreach(_source ${GOOGLETEST_SOURCES})
     set_source_files_properties(${_source} PROPERTIES GENERATED 1)
 endforeach()
 
 add_library(gtest ${GOOGLETEST_SOURCES})
+
+if (APPLE)
+    add_definitions(-DGTEST_USE_OWN_TR1_TUPLE)
+    add_definitions(-D__GLIBCXX__)
+endif (APPLE)
